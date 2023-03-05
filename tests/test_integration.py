a8571fa4ae13bc11a2cc8f0912b451db5175bc7a
jhnnsrs <jhnnsrs@gmail.com>
2023-03-05 16:57:40 +0100

    test docker compose stuff

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -69,7 +69,6 @@ def test_connection_x(environment):
 
     client = Herre(
         grant=ClientCredentialsGrant(
-
         base_url="http://localhost:8008/o",
         client_id="DSNwVKbSmvKuIUln36FmpWNVE2KrbS2oRX0ke8PJ",
         client_secret="Gp3VldiWUmHgKkIxZjL2aEjVmNwnSyIGHWbQJo6bWMDoIUlBqvUyoGWUWAe6jI3KRXDOsD13gkYVCZR0po1BLFO9QT4lktKODHDs0GyyJEzmIjkpEOItfdCC4zIa3Qzu"),
